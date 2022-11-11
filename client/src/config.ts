2471496895ade707758972b758a054bdc22363f7
Anthony Nyule <tonynyule@gmail.com>
2022-11-11 07:21:01 +0300

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'vo0z3kaxxh';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-xcq3pl7siowa4fbr.us.auth0.com',
+  clientId: 'JWx9jdDIifi6yRGtx7rb1hmgrTwWeuWQ',
+  callbackUrl: 'http://localhost:3000/callback'
+};
